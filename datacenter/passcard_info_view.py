41c4fd6867cc6a7d7b34cb55ad52f2acff1bcff5
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-12 14:45:27 +0300

    Initial commit

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,27 +1,6 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils.timezone import localtime
-from datetime import timezone, datetime, timedelta
-
-
-# def get_duration(visit):
-#     if visit.leaved_at:
-#         return localtime(visit.leaved_at) - localtime(visit.entered_at)
-#     else:
-#         return localtime(datetime.now(timezone.utc)) - localtime(visit.entered_at)
-
-
-# def format_duration(duration):
-#     if isinstance(duration, timedelta):
-#         return f'{int(duration.total_seconds() // 3600)}ч. {(duration.seconds // 60) % 60}м.'
-#     return duration.strftime("%d-%m-%Y, %H:%M:%S")
-
-
-# def is_visit_long(visit, minutes=60):
-#     if visit < timedelta(minutes=minutes):
-#         return False
-#     return True
 
 
 def passcard_info_view(request, passcode):
