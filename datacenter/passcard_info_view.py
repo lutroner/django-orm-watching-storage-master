f929e42871c55491cf09ddf5d183eaeab2ec5a4c
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-13 18:04:38 +0300

    Fix variable names

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,18 +4,18 @@ from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.get(passcode=passcode)
-    visits = Visit.objects.filter(passcard=passcard)
-    this_passcard_visits = []
-    for visit in visits:
+    current_passcard = Passcard.objects.get(passcode=passcode)
+    current_passcard_visits = Visit.objects.filter(passcard=current_passcard)
+    current_passcard_visits_serialized = []
+    for visit in current_passcard_visits:
         this_passcard_visit = {
             'entered_at': f'{visit.format_duration(visit.entered_at)}',
             'duration': f'{visit.format_duration(visit.get_duration(visit))}',
             'is_strange': visit.is_visit_long(visit.get_duration(visit), 20)
         }
-        this_passcard_visits.append(this_passcard_visit)
+        current_passcard_visits_serialized.append(this_passcard_visit)
     context = {
-        'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'passcard': current_passcard,
+        'this_passcard_visits': current_passcard_visits_serialized
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -15,17 +15,17 @@ def format_duration(duration):
 
 
 def storage_information_view(request):
-    non_closed_visits = []
-    current_visits = Visit.objects.filter(leaved_at=None)
-    for visit in current_visits:
+    non_closed_visits_serialized = []
+    non_closed_visits = Visit.objects.filter(leaved_at=None)
+    for visit in non_closed_visits:
         current_visitor = {
             'who_entered': f'{visit.passcard}',
             'entered_at': f'{format_duration(visit.entered_at)}',
             'duration': format_duration(get_duration(visit))
         }
-        non_closed_visits.append(current_visitor)
+        non_closed_visits_serialized.append(current_visitor)
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': non_closed_visits_serialized,
     }
     return render(request, 'storage_information.html', context)
