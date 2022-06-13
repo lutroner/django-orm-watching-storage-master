f0441fe12ed3f3e7b3cd1e7ab5a7ac49131eb7c0
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-13 18:10:55 +0300

    Get rid of extra function

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,17 +1,5 @@
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils.timezone import localtime
-from datetime import timezone, datetime, timedelta
-
-
-def get_duration(visit):
-    return localtime(localtime(datetime.now(timezone.utc))) - localtime(visit.entered_at)
-
-
-def format_duration(duration):
-    if isinstance(duration, timedelta):
-        return f'{int(duration.total_seconds() // 3600)}ч. {(duration.seconds // 60) % 60}м.'
-    return duration.strftime("%d-%m-%Y, %H:%M:%S")
 
 
 def storage_information_view(request):
@@ -20,8 +8,8 @@ def storage_information_view(request):
     for visit in non_closed_visits:
         current_visitor = {
             'who_entered': f'{visit.passcard}',
-            'entered_at': f'{format_duration(visit.entered_at)}',
-            'duration': format_duration(get_duration(visit))
+            'entered_at': f'{visit.format_duration(visit.entered_at)}',
+            'duration': visit.format_duration(visit.get_duration(visit))
         }
         non_closed_visits_serialized.append(current_visitor)
 
