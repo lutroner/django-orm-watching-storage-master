96421782eecb28cfab7981f0e0ace314d40ffa9c
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-16 22:58:44 +0300

    Add env.bool() to get boolean value

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = {'true': True, 'false': False}.get(env.str('DEBUG').lower(), False)
+DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
